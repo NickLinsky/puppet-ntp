fbb9805915769acbd8bd63b905435387303cb1f4
root <root@nicklinsky1.mylabserver.com>
2018-01-18 06:36:32 +0000

    fix some bugs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,14 @@
 class ntp::params {
-  $package_name = 'ntp'
-  $package_ensure = 'present'
-  $config_name    = 'ntp.conf'
-  $config_filemode    = '0664'
-  $servers = ['0.centos.post.ntp.org','1.centos.pool.ntp.org']
-  $service_ensure = 'running'
-  $service_running = true
+  $package_name       = 'ntp'
+  $package_ensure     = 'present'
+  $config_name        = 'ntp.conf'
+  $config_file_mode   = '0664'
+  $servers            = ['0.centos.post.ntp.org','1.centos.pool.ntp.org']
+  $service_ensure     = 'running'
+  $service_running    = true
+  $service_enable     = true
   $service_hasrestart = true
-  $service_hasstatus = true
+  $service_hasstatus  = true
 
   $service_name = $facts['os']['family'] ? {
     'Debian' => 'ntp',
